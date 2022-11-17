df04a943d919ea507962185f1f5fd02eeb1403c7
KCaarlos <115487880+KCaarlos@users.noreply.github.com>
2022-11-17 22:54:11 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,25 +3,26 @@ using namespace std;
 
 int main() {
    
-float a=0,b=0,x=0,y=0,z=0;
+float a=0,b=0,x=0,y=0;
+float z;  
    
-cin>>a>>b>>c>>d;
+cin>>a>>b>>x>>y;
    
    if(x<0 && y>0){
    
-       z=a*x-b*y;
+       z=(a*x)-(b*y);
       cout<<z<<endl;
 
   } else if (x>=0 && y<=0){
       
-       z=a*(x*x)-b*y;
+       z= ((a*x*x)-(b*y));
       cout<<z<<endl;
    
 }  
    
    else  {
       
-       z=a*x+b*(y*y);
+       z=((a*x)+(b*y*y));
       cout<<z<<endl;
    }
    
