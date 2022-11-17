69f2bf675873fa3776391494fe791f4c2f0fae92
KCaarlos <115487880+KCaarlos@users.noreply.github.com>
2022-11-17 22:03:49 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,28 @@
 using namespace std;
 
 int main() {
+   
+float a=0,b=0;
+Int x=0,y=0,z=0;
+   
+cin>>a>>b>>c>>d;
+   
+   if(x<0 && y>0){
+   
+       z=a*x-b*y;
+}
+   else if (x>=0 && y<=0){
+      
+       z=a*(x^2)-b*y;
+   
+}  
+   
+   else {
+      
+       z=a*x+b*(y*2);
+}  
+   
+  cout<<z<<endl;
+   
    return 0;
 }
